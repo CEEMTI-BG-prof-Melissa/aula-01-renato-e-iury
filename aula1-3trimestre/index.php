396ad06f63e6058a0cb14eae98fa807dd80cd6a3
Arthur e Renato <renato.dgirardelli@aluno.edu.es.gov.br>
2025-09-03 11:01:54 -0300

    Create index.php

---
+++ aula1-3trimestre/index.php
@@ -0,0 +1,11 @@
+ <html>
+     <head>
+        <title>Aula 01 php</title>
+    </head>
+    <body>
+          <?php
+              echo "Olá, Mundo!"; 
+              echo "Renato e Iury Guilherme";
+         ?>
+     </body>
+ </html>
