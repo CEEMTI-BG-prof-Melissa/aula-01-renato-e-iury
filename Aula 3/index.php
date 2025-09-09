5cb17527279c576ca63ae34dd0d4fa64f7050827
Arthur e Renato <renato.dgirardelli@aluno.edu.es.gov.br>
2025-09-09 11:10:30 -0300

    Create index.php

---
+++ Aula 3/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Aula 02 PHP</title>
+    </head>
+    <body>
+        <?php
+        $nome1="Iury Guilherme";
+        $nome2="Renato";
+
+        $n1=5;
+        $n2=10;
+        $soma= $n1+$n2;
+
+        echo "Meu nome é " .$nome1 ." e minha dupla é " .$nome2;
+        echo "<br> Meu nome é $nome1 e minha dupla é $nome2";
+
+        echo "<br> <br> A soma de $n1 com $n2 é $soma."
+
+        
+
+        ?>
+    </body>
+</html>
