d2e833adbf4d790724c264203084914a8b70c9c5
Arthur e Renato <renato.dgirardelli@aluno.edu.es.gov.br>
2025-09-10 10:57:36 -0300

    Update index.php

--- Aula 3/index.php
+++ Aula 3/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Aula 02 PHP</title>
+        <title>Aula 03 PHP</title>
     </head>
     <body>
         <?php
@@ -14,10 +14,19 @@
         echo "Meu nome é " .$nome1 ." e minha dupla é " .$nome2;
         echo "<br> Meu nome é $nome1 e minha dupla é $nome2";
 
-        echo "<br> <br> A soma de $n1 com $n2 é $soma."
+        echo "<br> <br> A soma de $n1 com $n2 é $soma.";
 
-        
+        echo "<br><br> O resultado da subtração de $n1 e $n2 é igual a: " .($n1-$n2) .".";
 
-        ?>
+        echo "<br><br> O resultado da multiplicação de $n1 e $n2 é igual a: " .($n1*$n2) .".";
+
+        echo "<br><br> O resultado da divisão de $n1 e $n2 é igual a: " .($n1/$n2) .".";
+
+        echo "<br><br> O resto de $n1 e $n2 é igual a: " .($n1%$n2) .".";
+
+
+
+
+       ?>
     </body>
 </html>
